6bad6bbc2f7337d1d09850bbc0d47c4e378cec4b
diyor-adm <admtv24.7@gmail.com>
2021-11-23 22:19:38 +0000

    tayyor

--- function.py
+++ function.py
@@ -4,12 +4,18 @@ def latin_text(number):
     text = ''
     i = 0
     for num_order in range(len(number), 0, -1):
-        if len(number) >= 4 and number[-4] == '0' and num_order == 4:
-            text += (num_text[str(num_order)][number[i]] + 'ming ')
-        else:
-            text += (num_text[str(num_order)][number[i]]) +' '
-        i += 1
+        try: 
+            if len(number) >= 4 and number[-4] == '0' and num_order == 4:
+                text += (num_text[str(num_order)][number[i]] + 'ming ')
+            else:
+                text += (num_text[str(num_order)][number[i]]) +' '
+            i += 1
+        except KeyError:
+            pass
+
     return text
 
 def translit(message):
     return to_cyrillic(message)
+
+
--- main.py
+++ main.py
@@ -1,31 +1,96 @@
-from telegram import user
 from function import latin_text, translit
-from settings.local_settings import TELEGRAM_TOKEN
 from translate import to_cyrillic
 # telegram
-from telegram.ext import Updater, Dispatcher, CommandHandler, callbackcontext, dispatcher
+from telegram.ext import Updater, CommandHandler, dispatcher,CallbackContext, MessageHandler
+from telegram.ext.filters import Filters
 from telegram.update import Update
-from settings import settings
+from googletrans import Translator
 
 
-updater = Updater(token=settings.TELEGRAM_TOKEN)
+updater = Updater(token='2103506248:AAEA1Tjw5rGHiRZyZKI96-DbhNGfskjijg4')
 
 
-def send_welcome(update: Update, context: callbackcontext):
-   update.message.reply_text('Salom!')
 
-dispatcher = updater.dispatcher
-dispatcher.add_handler(CommandHandler('start', send_welcome))
+def send_welcome(update: Update, context: CallbackContext):
+   update.message.reply_text('Assalomu alaykum, sonni matn ko`rinishiga o`tkazuvchi botga xush kelibsiz!')
+   update.message.reply_text("Iltimos kerakli matn tilini tanlagandan so`ng sonni kiriting.\nMisol uchun:\nO`zbekcha matn uchun - /uz 123\nKirillcha matn uchun - /kr 123\nRuscha matn uchun - /ru 123\nInglizcha matn uchun - /en 123\n\nBot yaratuvchisi haqida batafsil /about buyrug`i orqali bilib olishingiz mumkin!")
+def all_message(update: Update, context: CallbackContext):
+   update.message.reply_text("Iltimos kerakli matn tilini tanlagandan so`ng sonni kiriting.\nMisol uchun:\nO`zbekcha matn uchun - /uz 123\nKirillcha matn uchun - /kr 123\nRuscha matn uchun - /ru 123\nInglizcha matn uchun - /en 123")
 
-
-# @bot.message_handler(func=lambda msg: msg.text is not None)
 # def trans(message):
 #     msg = message.text
 #     def javob(msg): return latin_text(msg)
 #     def kirill(msg): return translit(latin_text(msg))
 #     all_text = f"O'zbekcha: {javob(msg)}\nKirillcha: {kirill(msg)}\n\n@diyoradm :)"
 #     bot.reply_to(message, text=all_text)
+def uz_text(update: Update, context: CallbackContext):
+    args = context.args
+    if len(args)==0:
+        update.message.reply_text('Hech bo`lmasa bitta son yozing!')
+    else:
+        javob = text_sum(args)
+        update.message.reply_text(javob)
+
+def text_sum(args):
+    print(args)
+    message = []
+    for i in args:
+        message +=i
+    def javob(message): return latin_text(message)
+    return javob(message)
+    
+        
+
+def kr_text(update: Update, context: CallbackContext):
+    args = context.args
+    if len(args)==0:
+        update.message.reply_text('Hech bo`lmasa bitta son yozing!')
+    else:
+        update.message.reply_text(to_cyrillic(text_sum(args)))
+
+def en_text(update: Update, context: CallbackContext):
+    args = context.args
+    if len(args)==0:
+        update.message.reply_text('Hech bo`lmasa bitta son yozing!')
+    else:
+        matn = text_sum(args)
+        translator = Translator()
+        matn = translator.translate(matn, dest='en').text
+        print(matn)
+        update.message.reply_text(matn)
+
+def ru_text(update: Update, context: CallbackContext):
+    args = context.args
+    if len(args)==0:
+        update.message.reply_text('Hech bo`lmasa bitta son yozing!')
+    else:
+        matn = text_sum(args)
+        translator = Translator()
+        matn = translator.translate(matn, dest='ru').text
+        print(matn)
+        update.message.reply_text(matn)
+
+def about(update: Update, context: CallbackContext):
+    update.message.reply_text('Ushbu bot Diyorbek Abduqodirov tomonidan ishlab chiqildi. \n@diyoradm')
+
+
+
+dispatcher = updater.dispatcher
+dispatcher.add_handler(CommandHandler('start', send_welcome))
+dispatcher.add_handler(CommandHandler('uz', uz_text))
+dispatcher.add_handler(CommandHandler('kr', kr_text))
+dispatcher.add_handler(CommandHandler('en', en_text))
+dispatcher.add_handler(CommandHandler('ru', ru_text))
+dispatcher.add_handler(CommandHandler('about', about))
+dispatcher.add_handler(MessageHandler(Filters.all, all_message))
+
+
+
 
 
 updater.start_polling()
 updater.idle()
+
+
+# def kirill(message): return translit(latin_text(message))
+# all_text = f"O'zbekcha: {javob(message)}\nKirillcha: {kirill(message)}\n\n@diyoradm :)"
